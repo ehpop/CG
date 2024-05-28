0b8c4b2972f8e2d0807cb5e00175398a88360b53
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2024-05-28 13:47:48 +0200

    zadanie 3

--- C1/zadanie3.py
+++ C1/zadanie3.py
@@ -101,52 +101,70 @@ def create_sphere(radius, lat_segments, lon_segments):
             indices.append(second + 1)
             indices.append(first + 1)
 
-    return np.array(vertices, dtype=np.float32), np.array(normals, dtype=np.float32), np.array(indices, dtype=np.uint32)
+    return (
+        np.array(vertices, dtype=np.float32),
+        np.array(normals, dtype=np.float32),
+        np.array(indices, dtype=np.uint32),
+    )
 
 
 def compile_shader_program():
     shader = compileProgram(
         compileShader(vertex_shader, GL_VERTEX_SHADER),
-        compileShader(fragment_shader, GL_FRAGMENT_SHADER)
+        compileShader(fragment_shader, GL_FRAGMENT_SHADER),
     )
     return shader
 
 
 materials = {
-    "metal": (np.array([0.25, 0.25, 0.25], dtype=np.float32),
-              np.array([0.4, 0.4, 0.4], dtype=np.float32),
-              np.array([0.774597, 0.774597, 0.774597], dtype=np.float32),
-              76.8),
-    "plastic": (np.array([0.0, 0.1, 0.06], dtype=np.float32),
-                np.array([0.0, 0.50980392, 0.50980392], dtype=np.float32),
-                np.array([0.50196078, 0.50196078, 0.50196078], dtype=np.float32),
-                32.0),
-    "wood": (np.array([0.19125, 0.0735, 0.0225], dtype=np.float32),
-             np.array([0.7038, 0.27048, 0.0828], dtype=np.float32),
-             np.array([0.256777, 0.137622, 0.086014], dtype=np.float32),
-             12.8),
-    "wall": (np.array([0.1, 0.1, 0.1], dtype=np.float32),
-             np.array([0.6, 0.6, 0.6], dtype=np.float32),
-             np.array([0.1, 0.1, 0.1], dtype=np.float32),
-             8.0)
+    "metal": (
+        np.array([0.25, 0.25, 0.25], dtype=np.float32),
+        np.array([0.5, 0.5, 0.5], dtype=np.float32),
+        np.array([0.774597, 0.774597, 0.774597], dtype=np.float32),
+        80.0,
+    ),
+    "plastic": (
+        np.array([0.0, 0.1, 0.06], dtype=np.float32),
+        np.array([0.0, 0.50980392, 0.50980392], dtype=np.float32),
+        np.array([0.50196078, 0.50196078, 0.50196078], dtype=np.float32),
+        32.0,
+    ),
+    "wood": (
+        np.array([0.19125, 0.0735, 0.0225], dtype=np.float32),
+        np.array([0.7038, 0.27048, 0.0828], dtype=np.float32),
+        np.array([0.256777, 0.137622, 0.086014], dtype=np.float32),
+        12.8,
+    ),
+    "wall": (
+        np.array([0.1, 0.1, 0.1], dtype=np.float32),
+        np.array([0.6, 0.6, 0.6], dtype=np.float32),
+        np.array([0.1, 0.1, 0.1], dtype=np.float32),
+        8.0,
+    ),
 }
 current_material = "metal"
 
 
 def change_material(shader, material):
     ambient, diffuse, specular, shininess = materials[material]
-    glUniform3fv(glGetUniformLocation(shader, 'ambient'), 1, ambient)
-    glUniform3fv(glGetUniformLocation(shader, 'diffuse'), 1, diffuse)
-    glUniform3fv(glGetUniformLocation(shader, 'specular'), 1, specular)
-    glUniform1f(glGetUniformLocation(shader, 'shininess'), shininess)
+    glUniform3fv(glGetUniformLocation(shader, "ambient"), 1, ambient)
+    glUniform3fv(glGetUniformLocation(shader, "diffuse"), 1, diffuse)
+    glUniform3fv(glGetUniformLocation(shader, "specular"), 1, specular)
+    glUniform1f(glGetUniformLocation(shader, "shininess"), shininess)
 
 
 def draw_text(x, y, text):
-    font = pygame.font.SysFont('arial', 28)
+    font = pygame.font.SysFont("arial", 28)
     text_surface = font.render(text, False, (0, 255, 66, 255)).convert_alpha()
     text_data = pygame.image.tostring(text_surface, "RGBA", True)
     glWindowPos2d(x, y)
-    glDrawPixels(text_surface.get_width(), text_surface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, text_data)
+    glDrawPixels(
+        text_surface.get_width(),
+        text_surface.get_height(),
+        GL_RGBA,
+        GL_UNSIGNED_BYTE,
+        text_data,
+    )
 
 
 def main():
@@ -154,7 +172,7 @@ def main():
     screen = pygame.display.set_mode((800, 600), DOUBLEBUF | OPENGL)
     pygame.display.set_caption("Phong Shading in PyOpenGL")
 
-    glClearColor(1.0, 1.0, 1.0, 1.0)
+    glClearColor(0.4, 0.9, 0.9, 1.0)
 
     glEnable(GL_DEPTH_TEST)
     glEnable(GL_BLEND)
@@ -181,12 +199,12 @@ def main():
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO)
-    position = glGetAttribLocation(shader, 'position')
+    position = glGetAttribLocation(shader, "position")
     glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 0, None)
     glEnableVertexAttribArray(position)
 
     glBindBuffer(GL_ARRAY_BUFFER, NBO)
-    normal = glGetAttribLocation(shader, 'normal')
+    normal = glGetAttribLocation(shader, "normal")
     glVertexAttribPointer(normal, 3, GL_FLOAT, GL_FALSE, 0, None)
     glEnableVertexAttribArray(normal)
 
@@ -261,14 +279,20 @@ def main():
 
         glUseProgram(shader)
 
-        glUniformMatrix4fv(glGetUniformLocation(shader, 'projection'), 1, GL_FALSE, projection)
-        glUniformMatrix4fv(glGetUniformLocation(shader, 'view'), 1, GL_FALSE, view)
-        glUniformMatrix4fv(glGetUniformLocation(shader, 'model'), 1, GL_FALSE, model)
+        glUniformMatrix4fv(
+            glGetUniformLocation(shader, "projection"), 1, GL_FALSE, projection
+        )
+        glUniformMatrix4fv(glGetUniformLocation(shader, "view"), 1, GL_FALSE, view)
+        glUniformMatrix4fv(glGetUniformLocation(shader, "model"), 1, GL_FALSE, model)
 
-        glUniform3fv(glGetUniformLocation(shader, 'lightPos'), 1, light_pos)
-        glUniform3fv(glGetUniformLocation(shader, 'viewPos'), 1, view_pos)
-        glUniform3fv(glGetUniformLocation(shader, 'lightColor'), 1, light_color)
-        glUniform3fv(glGetUniformLocation(shader, 'objectColor'), 1, np.array([1.0, 1.0, 1.0], dtype=np.float32))
+        glUniform3fv(glGetUniformLocation(shader, "lightPos"), 1, light_pos)
+        glUniform3fv(glGetUniformLocation(shader, "viewPos"), 1, view_pos)
+        glUniform3fv(glGetUniformLocation(shader, "lightColor"), 1, light_color)
+        glUniform3fv(
+            glGetUniformLocation(shader, "objectColor"),
+            1,
+            np.array([1.0, 1.0, 1.0], dtype=np.float32),
+        )
 
         change_material(shader, current_material)
 
